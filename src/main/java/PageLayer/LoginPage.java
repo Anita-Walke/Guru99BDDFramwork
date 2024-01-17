0e823dac92b17c08c0c44918a81bced31071fddd
monikanavale <monikanavale5@gmail.com>
2024-01-17 14:42:58 +0530

    Firstname test case added

--- src/main/java/PageLayer/LoginPage.java
+++ src/main/java/PageLayer/LoginPage.java
@@ -1,7 +1,25 @@
 package PageLayer;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import BaseLayer.BaseClass;
+import UtilLayer.Wait;
 
 public class LoginPage extends BaseClass{
 
+@FindBy(name="firstName")
+private WebElement fname;
+	
+	public LoginPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void enterFnameLnameAndPhone(String Fname)
+	{
+		Wait.sendKeys(fname, Fname);
+	}
+	
 }
--- src/test/java/StepDefination/StepDefinationClass.java
+++ src/test/java/StepDefination/StepDefinationClass.java
@@ -1,19 +1,24 @@
 package StepDefination;
 
 import BaseLayer.BaseClass;
-import BaseLayer.BaseClass;
+import PageLayer.LoginPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class StepDefinationClass extends BaseClass{
 	
+	public static LoginPage login;
+	
 	@Given("user is on login page")
 	public void user_is_on_login_page() {
 	   BaseClass.initialization();
 	}
-	@When("user enter valid firstname,lastname")
-	public void user_enter_valid_firstname_lastname() {
-	    System.out.println("hi");
+	@When("user enter valid firstname,lastname and Phone")
+	public void user_enter_valid_firstname_lastname_and_phone() {
+	   
+		LoginPage login = new LoginPage();
+		login.enterFnameLnameAndPhone("Monika");
+		
 	}
 
 
