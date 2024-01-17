aef2d78b642f5925578020fe1ae4fa668613407f
monikanavale <monikanavale5@gmail.com>
2024-01-17 16:58:51 +0530

    Mailing Information test case added

--- src/main/java/PageLayer/LoginPage.java
+++ src/main/java/PageLayer/LoginPage.java
@@ -5,20 +5,64 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import BaseLayer.BaseClass;
+import UtilLayer.HandleDropDown;
 import UtilLayer.Wait;
 
 public class LoginPage extends BaseClass{
+	
 	@FindBy(name="firstName")
 	 WebElement fname;
+	
+	@FindBy(name="lastName")
+	 WebElement lname;
+	
+	@FindBy(name="phone")
+	 WebElement phone;
+	
+	@FindBy(name="userName")
+	 WebElement email;
+	
+	@FindBy(name="address1")
+	 WebElement add;
 
+	@FindBy(name="city")
+	 WebElement city;
+
+	@FindBy(name="state")
+	 WebElement state;
+
+	@FindBy(name="postalCode")
+	 WebElement pcode;
+
+	@FindBy(name="country")
+	 WebElement country;
+
+	
+	
+	
 public LoginPage ()
 {
 	PageFactory.initElements(driver, this);
 }
 
-public void EneterFname(String Fname)
+public void enterContactInformation(String Fname,String Lname,String Phone,String Email)
 {
 Wait.sendKeys(fname, Fname);
+Wait.sendKeys(lname, Lname);
+Wait.sendKeys(phone, Phone);
+Wait.sendKeys(email, Email);
+
+}
+
+public void enterMailingInfo(String Add,String City,String State,String Pcode ,String Country)
+{
+  Wait.sendKeys(add,Add);
+  Wait.sendKeys(city,City);
+
+  Wait.sendKeys(state,State);
+   Wait.sendKeys(pcode,Pcode);
+
+   HandleDropDown.selectByVisibleText(country,Country);
 
 }
 }
